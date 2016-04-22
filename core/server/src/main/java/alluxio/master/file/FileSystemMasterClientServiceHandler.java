aba74e9419f40c9a498cbe6178cb0ff924df336a
Jiri Simsa <jiri.simsa@gmail.com>
2016-04-22 14:08:01 -0700

    Addressing review comments.

--- core/server/src/main/java/alluxio/master/file/FileSystemMasterClientServiceHandler.java
+++ core/server/src/main/java/alluxio/master/file/FileSystemMasterClientServiceHandler.java
@@ -107,8 +107,8 @@ public final class FileSystemMasterClientServiceHandler
   /**
    * {@inheritDoc}
    *
-   * @deprecated since version 1.1 and will be removed in version 2.0, use
-   * {@link #getStatus(String)} instead
+   * @deprecated since version 1.1 and will be removed in version 2.0
+   * @see {@link #getStatus(String)}
    */
   @Override
   @Deprecated
