d5e651e35d1fa20d520d972967e38840edbef434
Pei Sun <peis@alluxio.com>
2016-05-16 10:09:52 -0700

    Add copyright

--- job/src/test/java/alluxio/job/util/TimeSeriesTest.java
+++ job/src/test/java/alluxio/job/util/TimeSeriesTest.java
@@ -5,6 +5,15 @@ import alluxio.Constants;
 import org.junit.Assert;
 import org.junit.Test;
 
+/*
+ * Copyright (c) 2016 Alluxio, Inc. All rights reserved.
+ *
+ * This software and all information contained herein is confidential and proprietary to Alluxio,
+ * and is protected by copyright and other applicable laws in the United States and other
+ * jurisdictions. You may not use, modify, reproduce, distribute, or disclose this software without
+ * the express written permission of Alluxio.
+ */
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
