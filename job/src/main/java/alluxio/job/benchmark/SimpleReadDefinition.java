c4ac1a5822a698f6754f6e9d3cf98245fae4d3c8
Andrew Audibert <aaudibert10@gmail.com>
2016-06-01 15:38:45 -0700

    Fix SimpleRead multi-thread duration calculation

--- job/src/main/java/alluxio/job/benchmark/SimpleReadDefinition.java
+++ job/src/main/java/alluxio/job/benchmark/SimpleReadDefinition.java
@@ -99,9 +99,9 @@ public final class SimpleReadDefinition
     Preconditions.checkArgument(benchmarkThreadTimeList.size() == 1,
         "SimpleWrite only does one batch");
     // calc the average time
-    long totalTimeNS = 0;
+    long totalTimeNs = 0;
     for (long time : benchmarkThreadTimeList.get(0)) {
-      totalTimeNS += time;
+      totalTimeNs += time;
     }
     long totalBytes = 0;
     for (long bytes : mReadBytesQueue) {
@@ -109,10 +109,12 @@ public final class SimpleReadDefinition
     }
     // release the queue
     mReadBytesQueue = null;
-    double throughput =
-        (totalBytes / (double) Constants.MB) / (totalTimeNS / (double) Constants.SECOND_NANO);
-    double averageTimeMS = totalTimeNS / (double) benchmarkThreadTimeList.size()
-        / Constants.SECOND_NANO * Constants.SECOND_MS;
-    return new IOThroughputResult(throughput, averageTimeMS);
+
+    double averageThroughputBpns = totalBytes / (double) totalTimeNs;
+    double averageThroughputMbps = (averageThroughputBpns / Constants.MB) * Constants.SECOND_NANO;
+    double averageTimeNs = totalTimeNs / (double) config.getThreadNum();
+    double averageTimeMs = (averageTimeNs / Constants.SECOND_NANO) * Constants.SECOND_MS;
+
+    return new IOThroughputResult(averageThroughputMbps, averageTimeMs);
   }
 }
