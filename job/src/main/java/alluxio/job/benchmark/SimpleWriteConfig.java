e896006d89494aaf293923ab49ad292f9783a87c
Chaomin Yu <ifcharming@gmail.com>
2016-06-01 17:43:06 -0700

    [SMALLFIX] Set default hdfsReplication factor to 1.

--- job/src/main/java/alluxio/job/benchmark/SimpleWriteConfig.java
+++ job/src/main/java/alluxio/job/benchmark/SimpleWriteConfig.java
@@ -66,8 +66,8 @@ public class SimpleWriteConfig extends AbstractBenchmarkJobConfig {
     FormatUtils.parseSpaceSize(blockSize);
     mBlockSize = blockSize;
     mWriteType = WriteType.valueOf(writeType);
-    // Default HDFS replication factor is 3
-    mHdfsReplication = hdfsReplication > 0 ? (short) hdfsReplication : 3;
+    // Set default HDFS replication factor to 1 for Alluxio benchmark purpose.
+    mHdfsReplication = hdfsReplication > 0 ? (short) hdfsReplication : 1;
   }
 
   /**
