8d03e98a2799c1971ce22b0e12dda44f3683b98d
Rico Chiu <ricochiu@gmail.com>
2016-06-29 11:21:57 -0700

    fix throughput latency output formatting

--- job/src/main/java/alluxio/job/benchmark/ThroughputLatency.java
+++ job/src/main/java/alluxio/job/benchmark/ThroughputLatency.java
@@ -113,9 +113,9 @@ public class ThroughputLatency implements BenchmarkTaskResult {
    * @param printStream the print stream
    */
   public void outputForAutobot(PrintStream printStream) {
-    printStream.printf("ColumnNames: AverageThroughput, PeakThroughput, ThroughputStdDev,"
+    printStream.println("ColumnNames: AverageThroughput, PeakThroughput, ThroughputStdDev,"
         + "Latency50, Latency90, Latency99, Latency99_9, Latency99_99, LatencyStdDev, ErrorRatio");
-    printStream.printf("ColumnTypes: float, float, float, "
+    printStream.println("ColumnTypes: float, float, float, "
         + "int, int, int, int, int, float, float");
 
     TimeSeries.Summary summary = mThroughput.getSummary();
