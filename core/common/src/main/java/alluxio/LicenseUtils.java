2f02d2cb2ef723861dd18fd6b881772f34299271
Chaomin Yu <ifcharming@gmail.com>
2016-07-11 14:10:02 -0700

    checkstyle

--- core/common/src/main/java/alluxio/LicenseUtils.java
+++ core/common/src/main/java/alluxio/LicenseUtils.java
@@ -33,8 +33,8 @@ public final class LicenseUtils {
     try {
       expirationTime = formatter.parse(LicenseConstants.LICENSE_EXPIRATION_DATE).getTime();
     } catch (ParseException e) {
-      LOG.error("License expiration date {} can not be parsed as a date, format: yyyy-MM-dd. " +
-          "Exiting...", LicenseConstants.LICENSE_EXPIRATION_DATE);
+      LOG.error("License expiration date {} can not be parsed as a date, format: yyyy-MM-dd. "
+          + "Exiting...", LicenseConstants.LICENSE_EXPIRATION_DATE);
       System.exit(-1);
     }
     if (System.currentTimeMillis() > expirationTime) {
