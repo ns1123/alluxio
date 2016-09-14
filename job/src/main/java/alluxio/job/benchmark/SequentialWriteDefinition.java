680e0875ef5ba6e7a91954e2ce19c5825e7bdf15
Rico Chiu <ricochiu@gmail.com>
2016-09-13 17:19:11 -0700

    fix checkstyle

--- job/src/main/java/alluxio/job/benchmark/SequentialWriteDefinition.java
+++ job/src/main/java/alluxio/job/benchmark/SequentialWriteDefinition.java
@@ -30,7 +30,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Write files to Alluxio sequentially to test the writing performance as the number of files or
