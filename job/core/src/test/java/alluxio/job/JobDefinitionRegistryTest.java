b0c60b293609834e436326e440697f11c1facc69
Yupeng Fu <yupeng9@users.noreply.github.com>
2016-09-16 16:03:07 -0700

    Separated jobs out into enterprise and microbench project #2 (#234)
    * separated jobs out into enterprise and microbench project
    
    * set license check
    
    * addressed comments
    
    * removed unused test-jar publishing from microbench
    
    * addressed comments
    
    * addressed comments
    
    * fix the async persist handler
    
    * fix assemble dependency
    
    * fix style issue
    
    * increased the timeout for waiting for job status to 30s

---
+++ job/core/src/test/java/alluxio/job/JobDefinitionRegistryTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2016 Alluxio, Inc. All rights reserved.
+ *
+ * This software and all information contained herein is confidential and proprietary to Alluxio,
+ * and is protected by copyright and other applicable laws in the United States and other
+ * jurisdictions. You may not use, modify, reproduce, distribute, or disclose this software without
+ * the express written permission of Alluxio.
+ */
+
+package alluxio.job;
+
+import alluxio.exception.ExceptionMessage;
+import alluxio.job.exception.JobDoesNotExistException;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ * Unit tests for {@link JobDefinitionRegistry}.
+ */
+public final class JobDefinitionRegistryTest {
+  /**
+   * The exception expected to be thrown.
+   */
+  @Rule
+  public ExpectedException mThrown = ExpectedException.none();
+
+  @Test
+  public void getJobDefinitionTest() throws Exception {
+    JobDefinition<TestJobConfig, ?, ?> definition = JobDefinitionRegistry.INSTANCE
+        .getJobDefinition(new TestJobConfig("test"));
+    Assert.assertTrue(definition instanceof TestJobDefinition);
+  }
+
+  @Test
+  public void getNonexistingJobDefinitionTest() throws Exception {
+    DummyJobConfig jobConfig = new DummyJobConfig();
+
+    mThrown.expect(JobDoesNotExistException.class);
+    mThrown.expectMessage(
+        ExceptionMessage.JOB_DEFINITION_DOES_NOT_EXIST.getMessage(jobConfig.getName()));
+
+    JobDefinitionRegistry.INSTANCE.getJobDefinition(jobConfig);
+  }
+
+  class DummyJobConfig implements JobConfig {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public String getName() {
+      return "dummy";
+    }
+  }
+}
--- tests/src/test/java/alluxio/job/JobIntegrationTest.java
+++ tests/src/test/java/alluxio/job/JobIntegrationTest.java
@@ -96,6 +96,6 @@ public abstract class JobIntegrationTest {
           throw Throwables.propagate(e);
         }
       }
-    }, 10 * Constants.SECOND_MS);
+    }, 30 * Constants.SECOND_MS);
   }
 }
