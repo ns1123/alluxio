c0be922a74c100ab0e8af0c2ed5ba8488c41b0fe
Yupeng Fu <yupeng.fu@gmail.com>
2016-09-20 16:30:35 -0700

    changed to 2 min

--- tests/src/test/java/alluxio/job/JobIntegrationTest.java
+++ tests/src/test/java/alluxio/job/JobIntegrationTest.java
@@ -96,6 +96,6 @@ public abstract class JobIntegrationTest {
           throw Throwables.propagate(e);
         }
       }
-    }, 60 * Constants.SECOND_MS);
+    }, 120 * Constants.SECOND_MS);
   }
 }
