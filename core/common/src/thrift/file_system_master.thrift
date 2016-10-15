b03c0361e98f91e479a3815ee789ae93d44ebea3
Andrew Audibert <aaudibert10@gmail.com>
2016-10-14 18:21:29 -0700

    Fix conflicts in proto and thrift

--- core/common/src/thrift/file_system_master.thrift
+++ core/common/src/thrift/file_system_master.thrift
@@ -18,16 +18,11 @@ struct CreateFileTOptions {
   2: optional bool persisted
   3: optional bool recursive
   4: optional i64 ttl
-<<<<<<< HEAD
-  5: optional common.TTtlAction ttlAction
-||||||| merged common ancestors
-=======
-  5: optional common.TTtlAction ttlAction
   // ALLUXIO CS ADD
   1001: optional i32 replicationMax;
   1002: optional i32 replicationMin;
   // ALLUXIO CS END
->>>>>>> upstream/enterprise-1.3
+  5: optional common.TTtlAction ttlAction
 }
 
 struct MountTOptions {
@@ -82,16 +77,11 @@ struct FileInfo {
   21: string persistenceState
   22: bool mountPoint
   23: list<FileBlockInfo> fileBlockInfos
-<<<<<<< HEAD
-  24: common.TTtlAction ttlAction
-||||||| merged common ancestors
-=======
-  24: common.TTtlAction ttlAction
   // ALLUXIO CS ADD
   1001: i32 replicationMax;
   1002: i32 replicationMin;
   // ALLUXIO CS END
->>>>>>> upstream/enterprise-1.3
+  24: common.TTtlAction ttlAction
 }
 
 struct FileSystemCommand {
@@ -116,16 +106,11 @@ struct SetAttributeTOptions {
   5: optional string group
   6: optional i16 mode
   7: optional bool recursive
-<<<<<<< HEAD
-  8: optional common.TTtlAction ttlAction
-||||||| merged common ancestors
-=======
-  8: optional common.TTtlAction ttlAction
   // ALLUXIO CS ADD
   1001: optional i32 replicationMax;
   1002: optional i32 replicationMin;
   // ALLUXIO CS END
->>>>>>> upstream/enterprise-1.3
+  8: optional common.TTtlAction ttlAction
 }
 
 union FileSystemCommandOptions {
