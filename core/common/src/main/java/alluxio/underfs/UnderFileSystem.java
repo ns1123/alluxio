b8f2efcf2f1eced3ef3a858098cf2f1875aea422
Chaomin Yu <ifcharming@gmail.com>
2016-12-15 18:18:12 -0800

    [SMALLFIX] Change a spam warn msg to debug level (#393)

--- core/common/src/main/java/alluxio/underfs/UnderFileSystem.java
+++ core/common/src/main/java/alluxio/underfs/UnderFileSystem.java
@@ -83,7 +83,8 @@ public interface UnderFileSystem {
             perm.setOwnerFromLoginModule();
           }
         } catch (IOException e) {
-          LOG.warn("Failed to set user from login module or thrift client: ", e);
+          // Set to debug level because this is expected during master and workers start.
+          LOG.debug("Failed to set user from login module or thrift client: ", e);
         }
         // ALLUXIO CS END
         // ALLUXIO CS REPLACE
