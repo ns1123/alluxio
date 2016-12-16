8bf9fc33103f964fa4d047b4c3608395711ddf2f
Bin Fan <fanbin103@gmail.com>
2016-12-15 17:48:58 -0800

    Fix sync

--- tests/src/test/java/alluxio/client/FileOutStreamAsyncWriteJobIntegrationTest.java
+++ tests/src/test/java/alluxio/client/FileOutStreamAsyncWriteJobIntegrationTest.java
@@ -85,6 +85,7 @@ public final class FileOutStreamAsyncWriteJobIntegrationTest
     status = mFileSystem.getStatus(filePath);
     Assert.assertEquals(PersistenceState.PERSISTED.toString(), status.getPersistenceState());
 
-    checkWrite(filePath, UnderStorageType.ASYNC_PERSIST, length, length);
+    checkFileInAlluxio(filePath, length);
+    checkFileInUnderStorage(filePath, length);
   }
 }
