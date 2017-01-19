084bb2c940812cbb9110ae3cc8dcc9f76cbf776b
Jiri Simsa <jiri.simsa@gmail.com>
2017-01-18 17:12:24 -0800

    Various CLI related fixes.

--- shell/src/main/java/alluxio/shell/command/enterprise/JobLeaderCommand.java
+++ shell/src/main/java/alluxio/shell/command/enterprise/JobLeaderCommand.java
@@ -9,12 +9,13 @@
  * See the NOTICE file distributed with this work for information regarding copyright ownership.
  */
 
-package alluxio.shell.command;
+package alluxio.shell.command.enterprise;
 
 import alluxio.Configuration;
 import alluxio.MasterInquireClient;
 import alluxio.PropertyKey;
 import alluxio.client.file.FileSystem;
+import alluxio.shell.command.AbstractShellCommand;
 
 import org.apache.commons.cli.CommandLine;
 
