38036d111e4146ec0de84c0e46b804a1c19cb3d6
Jiri Simsa <jiri.simsa@gmail.com>
2017-01-27 11:44:06 -0800

    Fix a typo.

--- job/server/src/main/java/alluxio/master/AlluxioJobMaster.java
+++ job/server/src/main/java/alluxio/master/AlluxioJobMaster.java
@@ -42,7 +42,7 @@ public final class AlluxioJobMaster {
 
     if (!ConfigurationUtils.masterHostConfigured()) {
       System.out.println(String.format(
-          "Cannot run alluxio job worker; master hostname is not "
+          "Cannot run alluxio job master; master hostname is not "
               + "configured. Please modify %s to either set %s or configure zookeeper with "
               + "%s=true and %s=[comma-separated zookeeper master addresses]",
           Configuration.SITE_PROPERTIES, PropertyKey.MASTER_HOSTNAME.toString(),
@@ -52,7 +52,7 @@ public final class AlluxioJobMaster {
 
     if (!ConfigurationUtils.jobMasterHostConfigured()) {
       System.out.println(String.format(
-          "Cannot run alluxio job worker; job master hostname is not "
+          "Cannot run alluxio job master; job master hostname is not "
               + "configured. Please modify %s to either set %s or configure zookeeper with "
               + "%s=true and %s=[comma-separated zookeeper master addresses]",
           Configuration.SITE_PROPERTIES, PropertyKey.JOB_MASTER_HOSTNAME.toString(),
