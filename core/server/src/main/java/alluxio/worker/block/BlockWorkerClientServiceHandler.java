0af49069fa8f35de97f62df2eb8cd6bf08ed1329
Calvin Jia <jia.calvin@gmail.com>
2017-02-23 15:19:02 -0800

    Resolve conflicts for [ALLUXIO-2564] Improve entry/exit debug logging

--- core/server/src/main/java/alluxio/worker/block/BlockWorkerClientServiceHandler.java
+++ core/server/src/main/java/alluxio/worker/block/BlockWorkerClientServiceHandler.java
@@ -163,7 +163,6 @@ public final class BlockWorkerClientServiceHandler implements BlockWorkerClientS
    * @throws AlluxioTException if an Alluxio error occurs
    */
   @Override
-<<<<<<< HEAD:core/server/src/main/java/alluxio/worker/block/BlockWorkerClientServiceHandler.java
   // ALLUXIO CS REPLACE
   // public LockBlockResult lockBlock(final long blockId, final long sessionId)
   //     throws AlluxioTException {
@@ -171,18 +170,7 @@ public final class BlockWorkerClientServiceHandler implements BlockWorkerClientS
   public LockBlockResult lockBlock(final long blockId, final long sessionId,
       final alluxio.thrift.Capability capability) throws AlluxioTException {
     // ALLUXIO CS END
-    LOG.debug("Enter LockBlock. sessionId:{}, blockId:{}", sessionId, blockId);
-    LockBlockResult ret = RpcUtils.call(new RpcCallable<LockBlockResult>() {
-||||||| parent of 251825f... Merge pull request #4801 from calvinjia/alluxio-2564-2
-  public LockBlockResult lockBlock(final long blockId, final long sessionId)
-      throws AlluxioTException {
-    LOG.debug("Enter LockBlock. sessionId:{}, blockId:{}", sessionId, blockId);
-    LockBlockResult ret = RpcUtils.call(new RpcCallable<LockBlockResult>() {
-=======
-  public LockBlockResult lockBlock(final long blockId, final long sessionId)
-      throws AlluxioTException {
     return RpcUtils.callAndLog(LOG, new RpcCallable<LockBlockResult>() {
->>>>>>> 251825f... Merge pull request #4801 from calvinjia/alluxio-2564-2:core/server/worker/src/main/java/alluxio/worker/block/BlockWorkerClientServiceHandler.java
       @Override
       public LockBlockResult call() throws AlluxioException {
         // ALLUXIO CS ADD
@@ -276,22 +264,10 @@ public final class BlockWorkerClientServiceHandler implements BlockWorkerClientS
   //     final long initialBytes, final int writeTier) throws AlluxioTException, ThriftIOException {
   // ALLUXIO CS WITH
   public String requestBlockLocation(final long sessionId, final long blockId,
-<<<<<<< HEAD:core/server/src/main/java/alluxio/worker/block/BlockWorkerClientServiceHandler.java
       final long initialBytes, final int writeTier, final alluxio.thrift.Capability capability)
       throws AlluxioTException, ThriftIOException {
     // ALLUXIO CS END
-    LOG.debug("Enter RequestBlockLocation. sessionId:{}, blockId:{}, initialBytes:{}, writeTier:{}",
-        sessionId, blockId, initialBytes, writeTier);
-    String ret = RpcUtils.call(new RpcCallableThrowsIOException<String>() {
-||||||| parent of 251825f... Merge pull request #4801 from calvinjia/alluxio-2564-2
-      final long initialBytes, final int writeTier) throws AlluxioTException, ThriftIOException {
-    LOG.debug("Enter RequestBlockLocation. sessionId:{}, blockId:{}, initialBytes:{}, writeTier:{}",
-        sessionId, blockId, initialBytes, writeTier);
-    String ret = RpcUtils.call(new RpcCallableThrowsIOException<String>() {
-=======
-      final long initialBytes, final int writeTier) throws AlluxioTException, ThriftIOException {
     return RpcUtils.callAndLog(LOG, new RpcCallableThrowsIOException<String>() {
->>>>>>> 251825f... Merge pull request #4801 from calvinjia/alluxio-2564-2:core/server/worker/src/main/java/alluxio/worker/block/BlockWorkerClientServiceHandler.java
       @Override
       public String call() throws AlluxioException, IOException {
         // ALLUXIO CS ADD
@@ -408,7 +384,7 @@ public final class BlockWorkerClientServiceHandler implements BlockWorkerClientS
   @Override
   public void updateCapability(final alluxio.thrift.Capability capability)
       throws AlluxioTException {
-    RpcUtils.call(new RpcCallable<Void>() {
+    RpcUtils.call(LOG, new RpcCallable<Void>() {
       @Override
       public Void call() throws AlluxioException {
         mWorker.getCapabilityCache().addCapability(capability);
