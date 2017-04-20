be070284804a37564cc812b8a19c06d24a415f24
Calvin Jia <jia.calvin@gmail.com>
2017-04-20 14:48:23 -0700

    Add to decoder.

--- core/common/src/main/java/alluxio/network/protocol/RPCMessage.java
+++ core/common/src/main/java/alluxio/network/protocol/RPCMessage.java
@@ -47,6 +47,7 @@ public abstract class RPCMessage implements EncodedMessage {
     RPC_SASL_COMPLETE_RESPONSE(10),
     RPC_SECRET_KEY_WRITE_REQUEST(11),
     RPC_SECRET_KEY_WRITE_RESPONSE(12),
+    RPC_SASL_MESSAGE(13),
     // ALLUXIO CS END
 
     // Tags lower than 100 are reserved since v1.4.0.
@@ -126,6 +127,8 @@ public abstract class RPCMessage implements EncodedMessage {
           return RPC_SECRET_KEY_WRITE_REQUEST;
         case 12:
           return RPC_SECRET_KEY_WRITE_RESPONSE;
+        case 13:
+          return RPC_SASL_MESSAGE;
         // ALLUXIO CS END
         case 100:
           return RPC_READ_REQUEST;
@@ -225,6 +228,8 @@ public abstract class RPCMessage implements EncodedMessage {
         return RPCSecretKeyWriteRequest.decode(in);
       case RPC_SECRET_KEY_WRITE_RESPONSE:
         return RPCSecretKeyWriteResponse.decode(in);
+      case RPC_SASL_MESSAGE:
+        return RPCProtoMessage.decode(in, ProtoMessage.Type.SASL_MESSAGE);
       // ALLUXIO CS END
       case RPC_READ_REQUEST:
         return RPCProtoMessage.decode(in, ProtoMessage.Type.READ_REQUEST);
