1820027fe260f62d50ca75eb97b48312ebaef04d
Andrew Audibert <andrew@alluxio.com>
2017-04-26 12:56:06 -0700

    Throw AlluxioTException from all service methods

--- job/common/src/thrift/job_master.thrift
+++ job/common/src/thrift/job_master.thrift
@@ -92,12 +92,17 @@ service JobMasterWorkerService extends common.AlluxioService {
   /**
    * Periodic worker heartbeat returns a list of commands for the worker to execute.
    */
-  list<JobCommand> heartbeat(/** the id of the worker */ 1: i64 workerId,
-      /** the list of tasks status **/ 2: list<TaskInfo> taskInfoList)
-  throws (1: exception.AlluxioTException e)
+  list<JobCommand> heartbeat(
+    /** the id of the worker */ 1: i64 workerId,
+    /** the list of tasks status **/ 2: list<TaskInfo> taskInfoList
+    )
+    throws (1: exception.AlluxioTException e)
 
   /**
    * Returns a worker id for the given network address.
    */
-  i64 registerWorker( /** the worker network address */ 1: common.WorkerNetAddress workerNetAddress)
+  i64 registerWorker(
+    /** the worker network address */ 1: common.WorkerNetAddress workerNetAddress
+    )
+    throws (1: exception.AlluxioTException e)
 }
