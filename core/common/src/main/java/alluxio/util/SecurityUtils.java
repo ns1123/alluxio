8139e9a3e4915de3889e96e208048c46a1082fae
Andrew Audibert <andrew@alluxio.com>
2017-04-27 18:43:46 -0700

    Fix tests

--- core/common/src/main/java/alluxio/util/SecurityUtils.java
+++ core/common/src/main/java/alluxio/util/SecurityUtils.java
@@ -93,6 +93,10 @@ public final class SecurityUtils {
   public static String getGroupFromLoginModule() {
     try {
       return CommonUtils.getPrimaryGroupName(LoginUser.get().getName());
+    // ALLUXIO CS ADD
+    } catch (UnauthenticatedException e) {
+      return "";
+    // ALLUXIO CS END
     } catch (IOException | UnsupportedOperationException e) {
       return "";
     }
