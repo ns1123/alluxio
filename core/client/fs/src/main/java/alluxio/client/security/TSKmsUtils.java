15e1ab239dd5528816509fc675b0f27e91f67467
Chaomin Yu <ifcharming@gmail.com>
2017-05-18 22:47:18 -0700

    Address review comments.

--- core/client/fs/src/main/java/alluxio/client/security/TSKmsUtils.java
+++ core/client/fs/src/main/java/alluxio/client/security/TSKmsUtils.java
@@ -14,19 +14,19 @@ package alluxio.client.security;
 import alluxio.Constants;
 import alluxio.proto.security.EncryptionProto;
 
-import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-
-import javax.ws.rs.core.Response;
 
 /**
  * The utils to communicate with TwoSigma Key Management Service (KMS).
@@ -37,8 +37,9 @@ public final class TSKmsUtils {
   private static final String ENCRYPT_METHOD = "encrypt";
   private static final String DECRYPT_METHOD = "decrypt";
   private static final String KEY = "k";
-  private static final String TTL = "tt";
+  private static final String TTL = "ttl";
   private static final String DEFAULT_TTL_VALUE = "86400";
+  private static final int RESPONSE_BUFFER_SIZE = 8 * Constants.KB;
 
   /**
    * Gets the crypto key from KMS.
@@ -61,44 +62,39 @@ public final class TSKmsUtils {
 
   private static EncryptionProto.CryptoKey call(String kms, String op, Map<String, String> params)
       throws IOException {
-    HttpURLConnection connection = (HttpURLConnection) createURL(kms, op, params).openConnection();
-    connection.setRequestMethod("GET");
-    connection.setDoOutput(true);
-    connection.setRequestProperty("Content-Type", "application/octet-stream");
-
-    connection.connect();
-    if (connection.getResponseCode() != Response.Status.OK.getStatusCode()) {
-      InputStream errorStream = connection.getErrorStream();
-      if (errorStream != null) {
-        throw new IOException("Request failed: " + IOUtils.toString(errorStream));
+    CloseableHttpClient httpclient = HttpClients.createDefault();
+    HttpGet httpGet = new HttpGet(createURL(kms, op, params));
+    CloseableHttpResponse response = httpclient.execute(httpGet);
+    try {
+      if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
+        throw new IOException("Request failed with: " + response.getStatusLine().toString());
       }
-      throw new IOException("Request failed with status code " + connection.getResponseCode());
+      return getCryptoKey(response);
+    } finally {
+      response.close();
     }
-    return getResponse(connection);
   }
 
   /**
    * @return The URL which is created
    */
-  private static URL createURL(String kms, String op, Map<String, String> params)
+  private static String createURL(String kms, String op, Map<String, String> params)
       throws IOException {
     StringBuilder sb = new StringBuilder();
     for (Map.Entry<String, String> parameter : params.entrySet()) {
       sb.append(parameter.getKey() + "=" + parameter.getValue() + "&");
     }
-    return new URL(
-        "http://" + kms + Constants.KMS_API_PREFIX + "/" + op
-            + "?" + sb.toString());
+    return String.format("http://%s%s/%s?%s", kms, Constants.KMS_API_PREFIX, op, sb.toString());
   }
 
   /**
-   * @param connection the HttpURLConnection
+   * @param response the http response from KMS
    * @return the crypto key parsed from the InputStream of HttpURLConnection
    */
-  private static EncryptionProto.CryptoKey getResponse(HttpURLConnection connection)
+  private static EncryptionProto.CryptoKey getCryptoKey(CloseableHttpResponse response)
       throws IOException {
-    InputStream inputStream = connection.getInputStream();
-    byte[] buffer = new byte[8 * Constants.KB];
+    InputStream inputStream = response.getEntity().getContent();
+    byte[] buffer = new byte[RESPONSE_BUFFER_SIZE];
     int len = inputStream.read(buffer);
     EncryptionProto.CryptoKey key = EncryptionProto.CryptoKey.parseFrom(
         Arrays.copyOfRange(buffer, 0, len));
