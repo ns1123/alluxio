0d7017cb75d3f884087bc227872422206d11c42b
Pei Sun <peis@alluxio.com>
2017-06-02 11:25:35 -0700

    checkstyle fix

--- core/server/worker/src/main/java/alluxio/worker/netty/DataServerWriteHandler.java
+++ core/server/worker/src/main/java/alluxio/worker/netty/DataServerWriteHandler.java
@@ -35,7 +35,6 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.Closeable;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Queue;
