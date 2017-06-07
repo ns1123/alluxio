9da676cb64320cc1428382b8fde1ae2cb85cfca0
Chaomin Yu <ifcharming@gmail.com>
2017-06-07 13:35:28 -0700

    Fix build

--- core/client/fs/src/main/java/alluxio/client/netty/NettyClient.java
+++ core/client/fs/src/main/java/alluxio/client/netty/NettyClient.java
@@ -134,7 +134,7 @@ public final class NettyClient {
       }
     }
     // After 10 missed heartbeat attempts and no write activity, the server will close the channel.
-    final long timeoutMs = Configuration.getLong(PropertyKey.NETWORK_NETTY_HEARTBEAT_TIMEOUT_MS);
+    final long timeoutMs = Configuration.getMs(PropertyKey.NETWORK_NETTY_HEARTBEAT_TIMEOUT_MS);
     final long heartbeatPeriodMs = Math.max(timeoutMs / 10, 1);
     channel.pipeline()
         .addLast(new IdleStateHandler(0, heartbeatPeriodMs, 0, TimeUnit.MILLISECONDS));
