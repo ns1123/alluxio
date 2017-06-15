f77efd94927d25727d3273b62d2c3c4d40df0efb
Bin Fan <fanbin103@gmail.com>
2017-06-15 14:56:04 -0700

    Add MapRFS UFS implementation copied from HDFS, build can work

---
+++ underfs/maprfs/src/main/java/alluxio/underfs/maprfs/HdfsSecurityUtils.java
@@ -0,0 +1,112 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
+package alluxio.underfs.maprfs;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.security.UserGroupInformation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.security.PrivilegedExceptionAction;
+
+/**
+ * A utility class that allows program code to run in a security context provided by the HDFS
+ * security user and groups information.
+ */
+public final class HdfsSecurityUtils {
+  private static final Logger LOG = LoggerFactory.getLogger(HdfsSecurityUtils.class);
+
+  /** The HDFS configuration. */
+  private static Configuration sHdfsConf = new Configuration();
+
+  private static boolean isHdfsSecurityEnabled() {
+    return UserGroupInformation.isSecurityEnabled();
+  }
+
+  /**
+   * Runs a method in a security context as the current user.
+   *
+   * @param runner the method to be run
+   * @param <T> the return type
+   * @return the result of the secure method
+   * @throws IOException if failed to run as the current user
+   */
+  public static <T> T runAsCurrentUser(final SecuredRunner<T> runner) throws IOException {
+    UserGroupInformation.setConfiguration(sHdfsConf);
+    if (!isHdfsSecurityEnabled()) {
+      LOG.warn("security is not enabled");
+      return runner.run();
+    }
+
+    UserGroupInformation ugi = UserGroupInformation.getCurrentUser();
+    return runAs(ugi, runner);
+  }
+
+  /**
+   * Runs a method in a security context as the specified user.
+   *
+   * @param runner the method to be run
+   * @param <T> the return type
+   * @param ugi the specified user
+   * @return the result of the secure method
+   * @throws IOException if failed to run as the specified user
+   */
+  public static <T> T runAs(UserGroupInformation ugi, final SecuredRunner<T> runner)
+      throws IOException {
+    UserGroupInformation.setConfiguration(sHdfsConf);
+    if (!isHdfsSecurityEnabled()) {
+      LOG.warn("security is not enabled");
+      return runner.run();
+    }
+
+    ugi.setConfiguration(sHdfsConf);
+
+    LOG.debug("UGI: {}", ugi.toString());
+    LOG.debug("UGI login user {}", ugi.getLoginUser());
+    LOG.debug("UGI current user {}", ugi.getCurrentUser());
+
+    if (ugi.getAuthenticationMethod() == UserGroupInformation.AuthenticationMethod.KERBEROS
+        && !ugi.hasKerberosCredentials()) {
+      LOG.error("UFS Kerberos security is enabled but UGI has no Kerberos credentials. "
+          + "Please check Alluxio configurations for Kerberos principal and keytab file.");
+    }
+    try {
+      return ugi.doAs(new PrivilegedExceptionAction<T>() {
+        @Override
+        public T run() throws IOException {
+          return runner.run();
+        }
+      });
+    } catch (InterruptedException e) {
+      throw new IOException(e);
+    }
+  }
+
+  /**
+   * Interface for specifying logic to execute securely.
+   *
+   * @param <T> the return type of run method
+   */
+  public interface SecuredRunner<T> {
+    /**
+     * Program logic to execute securely.
+     *
+     * @return an instance of {@code T}
+     * @throws IOException if something went wrong
+     */
+    T run() throws IOException;
+  }
+
+  private HdfsSecurityUtils() {}  // prevent instantiation
+}
+
