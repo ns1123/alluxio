b79d8e33b860a47fdd7b688429cc3eb13ba55c5d
Bin Fan <fanbin103@gmail.com>
2017-07-18 20:37:45 -0700

    [MERGE] Fix build

--- core/common/src/test/java/alluxio/security/UserTest.java
+++ core/common/src/test/java/alluxio/security/UserTest.java
@@ -85,7 +85,7 @@ public final class UserTest {
     try {
       Subject subject = new Subject();
       User user = new User(subject);
-      Assert.fail("creating User from an empty subject should fail");
+      org.junit.Assert.fail("creating User from an empty subject should fail");
     } catch (Exception e) {
       // Expected
     }
@@ -104,29 +104,29 @@ public final class UserTest {
     subject.getPrincipals().add(
         new javax.security.auth.kerberos.KerberosPrincipal("foo/admin@" + TEST_REALM));
     User user = new User(subject);
-    Assert.assertNotNull(user.getSubject());
-    Assert.assertEquals("[foo/admin@EXAMPLE.COM]",
+    org.junit.Assert.assertNotNull(user.getSubject());
+    assertEquals("[foo/admin@EXAMPLE.COM]",
         user.getSubject().getPrincipals(
             javax.security.auth.kerberos.KerberosPrincipal.class).toString());
-    Assert.assertEquals("foo", user.getName());
+    assertEquals("foo", user.getName());
 
     // Test equals.
-    Assert.assertTrue(user.equals(user));
-    Assert.assertFalse(user.equals(null));
+    assertTrue(user.equals(user));
+    assertFalse(user.equals(null));
 
     // Two principal in subject, for now User only takes the first principal as the user name.
     subject.getPrincipals().add(
         new javax.security.auth.kerberos.KerberosPrincipal("bar/admin@" + TEST_REALM));
     user = new User(subject);
-    Assert.assertNotNull(user.getSubject());
-    Assert.assertEquals(String.format("[foo/admin@%s, bar/admin@%s]", TEST_REALM, TEST_REALM),
+    org.junit.Assert.assertNotNull(user.getSubject());
+    assertEquals(String.format("[foo/admin@%s, bar/admin@%s]", TEST_REALM, TEST_REALM),
         user.getSubject().getPrincipals(
             javax.security.auth.kerberos.KerberosPrincipal.class).toString());
-    Assert.assertEquals("foo", user.getName());
+    assertEquals("foo", user.getName());
 
     // Test Equals.
-    Assert.assertTrue(user.equals(user));
-    Assert.assertFalse(user.equals(null));
+    assertTrue(user.equals(user));
+    assertFalse(user.equals(null));
   }
   // ALLUXIO CS END
 }
