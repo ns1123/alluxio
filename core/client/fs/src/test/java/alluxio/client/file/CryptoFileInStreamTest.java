f79ba89300b0f69757dd317fd788f5359f7d8838
Yupeng Fu <yupeng.fu@gmail.com>
2017-08-31 10:20:09 -0700

    resolve conflicts

--- core/client/fs/src/test/java/alluxio/client/file/CryptoFileInStreamTest.java
+++ core/client/fs/src/test/java/alluxio/client/file/CryptoFileInStreamTest.java
@@ -19,6 +19,7 @@ import alluxio.client.ReadType;
 import alluxio.client.block.AlluxioBlockStore;
 import alluxio.client.block.BlockWorkerInfo;
 import alluxio.client.block.stream.BlockInStream;
+import alluxio.client.block.stream.BlockInStream.BlockInStreamSource;
 import alluxio.client.block.stream.BlockOutStream;
 import alluxio.client.block.stream.TestBlockInStream;
 import alluxio.client.block.stream.TestBlockOutStream;
@@ -151,7 +152,8 @@ public final class CryptoFileInStreamTest {
             public BlockInStream answer(InvocationOnMock invocation) throws Throwable {
               long i = (Long) invocation.getArguments()[0];
               byte[] input = getBlockData((int) i);
-              return new TestBlockInStream(input, i, input.length, false);
+              return new TestBlockInStream(input, i, input.length, false,
+                  BlockInStreamSource.LOCAL);
             }
           });
       Mockito.when(mBlockStore.getOutStream(Mockito.eq((long) i), Mockito.anyLong(),
