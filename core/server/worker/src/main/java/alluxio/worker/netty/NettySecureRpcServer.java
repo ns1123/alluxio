6d67b4404cae0fb37450752950ef5bc7664872d3
Andrew Audibert <andrew@alluxio.com>
2017-09-08 00:12:32 -0700

    Fix netty tests

--- core/server/worker/src/main/java/alluxio/worker/netty/NettySecureRpcServer.java
+++ core/server/worker/src/main/java/alluxio/worker/netty/NettySecureRpcServer.java
@@ -104,7 +104,7 @@ public final class NettySecureRpcServer {
   public void close() throws IOException {
     int quietPeriodSecs =
         Configuration.getInt(PropertyKey.WORKER_NETWORK_NETTY_SHUTDOWN_QUIET_PERIOD);
-    int timeoutSecs = Configuration.getInt(PropertyKey.WORKER_NETWORK_NETTY_SHUTDOWN_TIMEOUT);
+    long timeoutSecs = Configuration.getMs(PropertyKey.WORKER_NETWORK_NETTY_SHUTDOWN_TIMEOUT);
 
     // The following steps are needed to shut down the secure RPC server:
     //
