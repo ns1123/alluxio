411b2a708c9f5003a3cc5eec94409d697834eaa6
Rico Chiu <ricochiu@gmail.com>
2017-12-20 17:09:29 -0800

    cmon GOPATH

--- dev/scripts/check-annotation.sh
+++ dev/scripts/check-annotation.sh
@@ -1,8 +1,6 @@
-#!/usr/bin/env bash
-
-set -e
+#!/bin/bash
 
 SCRIPTS_DIR="$(cd "$(dirname "$0" )"; pwd)"
 REPO_ROOT=$SCRIPTS_DIR/../..
-GOPATH=$SCRIPTS_DIR; go run $GOPATH/src/alluxio.com/annotation/main.go -repo $REPO_ROOT lint -fail-on-warning
+GOPATH=$SCRIPTS_DIR go run $SCRIPTS_DIR/src/alluxio.com/annotation/main.go -repo $REPO_ROOT lint -fail-on-warning
 
