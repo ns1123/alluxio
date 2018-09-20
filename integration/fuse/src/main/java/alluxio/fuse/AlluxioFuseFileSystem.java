a3a546c485792d577d6fdafc6ef3678e3df79e39
Lu Qiu <luqiujob@gmail.com>
2018-09-20 11:24:24 -0700

    fix code style

--- integration/fuse/src/main/java/alluxio/fuse/AlluxioFuseFileSystem.java
+++ integration/fuse/src/main/java/alluxio/fuse/AlluxioFuseFileSystem.java
@@ -52,7 +52,6 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeoutException;
 
 import javax.annotation.concurrent.ThreadSafe;
 
