7f7e0c1a4f2399af67cacbaa96083acb89d405f0
Gokturk Gezer <gokturk@alluxio.com>
2019-03-06 12:33:48 -0800

    Restore per-test initialization of resources in PrivilegeMasterTest

--- core/server/master/src/test/java/alluxio/master/privilege/PrivilegeMasterTest.java
+++ core/server/master/src/test/java/alluxio/master/privilege/PrivilegeMasterTest.java
@@ -27,9 +27,9 @@ import alluxio.proto.journal.Privilege.PrivilegeUpdateEntry;
 import alluxio.wire.Privilege;
 
 import com.google.common.collect.ImmutableMap;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
@@ -43,83 +43,81 @@ import java.util.Set;
  * Unit tests for {@link PrivilegeMaster}.
  */
 public final class PrivilegeMasterTest {
-  private static PrivilegeMaster sMaster;
-  private static MasterRegistry sRegistry;
-  private static JournalSystem sJournalSystem;
+  private JournalSystem mJournalSystem;
+  private PrivilegeMaster mMaster;
+  private MasterRegistry mRegistry;
 
   /** Rule to create a new temporary folder during each test. */
-  @ClassRule
-  public static TemporaryFolder sTestFolder = new TemporaryFolder();
+  @Rule
+  public TemporaryFolder mTestFolder = new TemporaryFolder();
 
-  @BeforeClass
-  public static void before() throws Exception {
+  @Before
+  public void before() throws Exception {
+    mRegistry = new MasterRegistry();
     // To make sure Raft cluster and connect address match.
     ServerConfiguration.set(PropertyKey.MASTER_HOSTNAME, "localhost");
-    sRegistry = new MasterRegistry();
-    sJournalSystem = JournalTestUtils.createJournalSystem(sTestFolder);
-    sMaster = new PrivilegeMasterFactory().create(sRegistry,
-        MasterTestUtils.testMasterContext(sJournalSystem));
-
-    sJournalSystem.start();
-    sJournalSystem.gainPrimacy();
-    sRegistry.start(true);
+    mJournalSystem = JournalTestUtils.createJournalSystem(mTestFolder);
+    mMaster = new PrivilegeMasterFactory().create(mRegistry,
+        MasterTestUtils.testMasterContext(mJournalSystem));
+    mJournalSystem.start();
+    mJournalSystem.gainPrimacy();
+    mRegistry.start(true);
   }
 
-  @AfterClass
-  public static void after() throws Exception {
-    sJournalSystem.stop();
-    sRegistry.stop();
+  @After
+  public void after() throws Exception {
+    mJournalSystem.stop();
+    mRegistry.stop();
   }
 
   @Test
   public void processGrantAndRevokeJournalEntry() throws Exception {
-    sMaster.processJournalEntry(JournalEntry.newBuilder().setPrivilegeUpdate(
+    mMaster.processJournalEntry(JournalEntry.newBuilder().setPrivilegeUpdate(
         PrivilegeUpdateEntry.newBuilder()
         .setGroup("testGroup")
         .setGrant(true)
         .addAllPrivilege(Arrays.asList(PPrivilege.FREE_PRIVILEGE)))
         .build());
-    assertTrue(sMaster.hasPrivilege("testGroup", Privilege.FREE));
-    sMaster.processJournalEntry(JournalEntry.newBuilder().setPrivilegeUpdate(
+    assertTrue(mMaster.hasPrivilege("testGroup", Privilege.FREE));
+    mMaster.processJournalEntry(JournalEntry.newBuilder().setPrivilegeUpdate(
         PrivilegeUpdateEntry.newBuilder()
         .setGroup("testGroup")
         .setGrant(false)
         .addAllPrivilege(Arrays.asList(PPrivilege.FREE_PRIVILEGE)))
         .build());
-    assertFalse(sMaster.hasPrivilege("testGroup", Privilege.FREE));
+    assertFalse(mMaster.hasPrivilege("testGroup", Privilege.FREE));
   }
 
   @Test
   public void hasPrivilege() throws Exception {
-    sMaster.updatePrivileges("testGroup", Arrays.asList(Privilege.FREE, Privilege.TTL), true);
-    assertTrue(sMaster.hasPrivilege("testGroup", Privilege.FREE));
-    assertTrue(sMaster.hasPrivilege("testGroup", Privilege.TTL));
-    assertFalse(sMaster.hasPrivilege("testGroup", Privilege.REPLICATION));
-    assertFalse(sMaster.hasPrivilege("testGroup", Privilege.PIN));
+    mMaster.updatePrivileges("testGroup", Arrays.asList(Privilege.FREE, Privilege.TTL), true);
+    assertTrue(mMaster.hasPrivilege("testGroup", Privilege.FREE));
+    assertTrue(mMaster.hasPrivilege("testGroup", Privilege.TTL));
+    assertFalse(mMaster.hasPrivilege("testGroup", Privilege.REPLICATION));
+    assertFalse(mMaster.hasPrivilege("testGroup", Privilege.PIN));
   }
 
   @Test
   public void otherGroupHasNoPrivilege() throws Exception {
     for (Privilege p : Privilege.values()) {
-      assertFalse(sMaster.hasPrivilege("nonexist", p));
+      assertFalse(mMaster.hasPrivilege("nonexist", p));
     }
   }
 
   @Test
   public void getPrivileges() throws Exception {
     List<Privilege> privileges = Arrays.asList(Privilege.FREE, Privilege.TTL);
-    sMaster.updatePrivileges("testGroup", privileges, true);
-    assertEquals(new HashSet<>(privileges), sMaster.getPrivileges("testGroup"));
+    mMaster.updatePrivileges("testGroup", privileges, true);
+    assertEquals(new HashSet<>(privileges), mMaster.getPrivileges("testGroup"));
   }
 
   @Test
   public void getGroupToPrivilegesMapping() throws Exception {
-    sMaster.resetState();
-    sMaster.updatePrivileges("group1", Arrays.asList(Privilege.FREE), true);
-    sMaster.updatePrivileges("group2", Arrays.asList(Privilege.TTL, Privilege.FREE), true);
+    mMaster.updatePrivileges("group1", Arrays.asList(Privilege.FREE), true);
+    mMaster.updatePrivileges("group2", Arrays.asList(Privilege.TTL, Privilege.FREE), true);
     Map<String, Set<Privilege>> expected = ImmutableMap.<String, Set<Privilege>>of(
         "group1", new HashSet<>(Arrays.asList(Privilege.FREE)),
         "group2", new HashSet<>(Arrays.asList(Privilege.TTL, Privilege.FREE)));
-    assertEquals(expected, sMaster.getGroupToPrivilegesMapping());
+    assertEquals(expected, mMaster.getGroupToPrivilegesMapping());
   }
 }
