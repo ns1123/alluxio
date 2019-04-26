61e5ed856505dc026a664e6a394dd0904379505f
Lu Qiu <luqiujob@gmail.com>
2019-04-26 16:06:53 -0700

    solve conflicts

--- tests/src/test/java/alluxio/client/hadoop/FileSystemUriIntegrationTest.java
+++ tests/src/test/java/alluxio/client/hadoop/FileSystemUriIntegrationTest.java
@@ -35,11 +35,7 @@ import java.net.URI;
  */
 public class FileSystemUriIntegrationTest extends BaseIntegrationTest {
   private static final int WAIT_TIMEOUT_MS = 60 * Constants.SECOND_MS;
-<<<<<<< HEAD
-  public MultiProcessCluster mCluster;
-=======
   private MultiProcessCluster mCluster;
->>>>>>> OPENSOURCE/master
 
   @After
   public void after() throws Exception {
@@ -48,11 +44,7 @@ public class FileSystemUriIntegrationTest extends BaseIntegrationTest {
 
   @Test
   public void zookeeperUriTest() throws Exception {
-<<<<<<< HEAD
-    mCluster = MultiProcessCluster.newBuilder(PortCoordination.MULTI_PROCESS_ZOOKEEPER)
-=======
     mCluster = MultiProcessCluster.newBuilder(PortCoordination.ZOOKEEPER_URI)
->>>>>>> OPENSOURCE/master
         .setClusterName("ZookeeperUriFileSystemIntegrationTest")
         .setNumMasters(3)
         .setNumWorkers(2)
@@ -64,39 +56,12 @@ public class FileSystemUriIntegrationTest extends BaseIntegrationTest {
         (ZkMasterConnectDetails) mCluster.getMasterInquireClient().getConnectDetails();
     String zkAddress = connectDetails.getZkAddress();
 
-<<<<<<< HEAD
-    Configuration conf = new Configuration();
-    conf.set("fs.alluxio.impl", FileSystem.class.getName());
-
-    URI uri = URI.create("alluxio://zk@" + zkAddress + "/tmp/path.txt");
-    org.apache.hadoop.fs.FileSystem fs = org.apache.hadoop.fs.FileSystem.get(uri, conf);
-
-    mCluster.waitForAllNodesRegistered(WAIT_TIMEOUT_MS);
-
-    Path file = new Path("/testFile-Zookeeper");
-    FsPermission permission = FsPermission.createImmutable((short) 0666);
-    FSDataOutputStream o = fs.create(file, permission, false /* ignored */, 10 /* ignored */,
-        (short) 1 /* ignored */, 512 /* ignored */, null /* ignored */);
-    o.writeBytes("Test Bytes");
-    o.close();
-    // with mark of delete-on-exit, the close method will try to delete it
-    fs.deleteOnExit(file);
-    fs.close();
-    mCluster.notifySuccess();
-  }
-  // ALLUXIO CS ADD
-
-  @Test
-  public void multiMasterUriTest() throws Exception {
-    mCluster = MultiProcessCluster.newBuilder(PortCoordination.MULTI_PROCESS_JOURNAL)
-=======
     testConnection("zk@" + zkAddress);
   }
 
   @Test
   public void multiMasterUriTest() throws Exception {
     mCluster = MultiProcessCluster.newBuilder(PortCoordination.MULTI_MASTER_URI)
->>>>>>> OPENSOURCE/master
         .setClusterName("MultiMastersUriFileSystemIntegrationTest")
         .setNumMasters(3)
         .setNumWorkers(1)
@@ -108,12 +73,6 @@ public class FileSystemUriIntegrationTest extends BaseIntegrationTest {
         .map(a -> (a.getHostname() + ":" + a.getRpcPort()))
         .collect(java.util.stream.Collectors.joining(","));
 
-<<<<<<< HEAD
-    Configuration conf = new Configuration();
-    conf.set("fs.alluxio.impl", FileSystem.class.getName());
-
-    URI uri = URI.create("alluxio://" + address + "/tmp/path.txt");
-=======
     testConnection(address);
   }
 
@@ -126,16 +85,11 @@ public class FileSystemUriIntegrationTest extends BaseIntegrationTest {
     Configuration conf = new Configuration();
     conf.set("fs.alluxio.impl", FileSystem.class.getName());
     URI uri = URI.create("alluxio://" + authority + "/tmp/path.txt");
->>>>>>> OPENSOURCE/master
     org.apache.hadoop.fs.FileSystem fs = org.apache.hadoop.fs.FileSystem.get(uri, conf);
 
     mCluster.waitForAllNodesRegistered(WAIT_TIMEOUT_MS);
 
-<<<<<<< HEAD
-    Path file = new Path("/testFile-MultiMaster");
-=======
     Path file = new Path("/testFile");
->>>>>>> OPENSOURCE/master
     FsPermission permission = FsPermission.createImmutable((short) 0666);
     FSDataOutputStream o = fs.create(file, permission, false /* ignored */, 10 /* ignored */,
         (short) 1 /* ignored */, 512 /* ignored */, null /* ignored */);
@@ -146,8 +100,4 @@ public class FileSystemUriIntegrationTest extends BaseIntegrationTest {
     fs.close();
     mCluster.notifySuccess();
   }
-<<<<<<< HEAD
-  // ALLUXIO CS END
-=======
->>>>>>> OPENSOURCE/master
 }
